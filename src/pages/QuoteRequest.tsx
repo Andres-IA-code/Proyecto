4c15c1e3cd3cfe9de07535187934e9cc7f40e3c8
Andres-IA-code <aconsiglio740@gmail.com>
2025-07-07 14:51:39 -0300

    Remove Fecha Flexible label

--- src/pages/QuoteRequest.tsx
+++ src/pages/QuoteRequest.tsx
@@ -661,11 +661,6 @@ const QuoteRequest: React.FC = () => {
                     className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded mr-2"
                   />
                   <label className="text-sm text-gray-700">
-                    Fecha Flexible
-                  </label>
-                  <label className="text-sm font-bold text-gray-700">
-                    Fecha Flexible
-                  </label>
                 </div>
               </div>
               {formData.flexibleDate && (
