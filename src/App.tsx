abdf6370315f76c174b9adcd89ee4fc1c36b2401
Andres-IA-code <aconsiglio740@gmail.com>
2025-09-05 16:08:16 -0300

    Create Privacy Policy page with data protection information

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import OperadorLogistico from './pages/OperadorLogistico';
 import BrokerLogistico from './pages/BrokerLogistico';
 import Solutions from './pages/Solutions';
 import Legal from './pages/Legal';
+import Privacy from './pages/Privacy';
 
 
 // Operador Logístico Pages
@@ -51,6 +52,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/legal" element={<Legal />} />
+        <Route path="/privacy" element={<Privacy />} />
         
         {/* Operador Logístico Routes */}
         <Route path="/operador" element={<OperadorLogistico />}>
