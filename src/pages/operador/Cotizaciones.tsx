10c2a34b0f6eaa708df21dae0eb301d5d5b85d0a
Andres-IA-code <aconsiglio740@gmail.com>
2025-09-07 16:36:30 -0300

    Remove Iniciar Viaje button

--- src/pages/operador/Cotizaciones.tsx
+++ src/pages/operador/Cotizaciones.tsx
@@ -638,11 +638,6 @@ const OperadorCotizaciones: React.FC = () => {
                 >
                   Cerrar
                 </button>
-                {selectedQuote.Estado?.toLowerCase() === 'aceptada' && (
-                  <button className="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors">
-                    Iniciar Viaje
-                  </button>
-                )}
               </div>
             </div>
           </div>
